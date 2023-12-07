074f8e4276984762f3792f993f49dd5563df832d
Farrah Metcalfe <farrah@localhost.localdomain>
2023-12-07 14:45:28 +0800

    init

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Assumes haxe is installed
+haxelib --global update haxelib
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib run lime setup flixel
+haxelib run lime setup
+haxelib install flixel-tools
+haxelib run flixel-tools setup
+
